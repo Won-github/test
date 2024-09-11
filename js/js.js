1922ddacd789424d63a7999c64cbde2c7a57143a
Won-github <tmddus638@naver.com>
2024-09-11 11:21:25 +0900

    Delete js directory

--- js/js.js
+++
@@ -1,9 +0,0 @@
-var jsImport = [
-    "../js/jquery-1.12.4.min.js",
-    "../js/jquery-ui.js",
-    "../js/common.js",
-    "../js/bootstrap.js",
-];
-for(var i=0; i < jsImport.length; i++){
-    document.write('<script type="text/javascript" src="'+ jsImport[i] + '"></script>');
-}
